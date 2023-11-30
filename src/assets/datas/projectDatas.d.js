68d9074da4dd2918c6e37c4991a6373b2355e950
CaquevelleLudovic <l.caquevelle@gmail.com>
2023-11-30 11:04:49 +0100

    add datas for the work page

---
+++ src/assets/datas/projectDatas.d.js
@@ -0,0 +1,9 @@
+export const projectDatasTab = [
+    {
+        id: 1,
+        title: "Nearby",
+        tools: "React / Symphony / Redux / Sass",
+        url: "https://projet-nearby-front.vercel.app/",
+    },
+];
+
